7fb9ccbf20cb1cfb6ecaa0ea6e973706103abca1
zethra <jediben97@gmail.com>
2015-08-10 17:15:33 -0400

    Added GPIO

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import printer
+import RPi.GPIO as GPIO
+import os
+import sys
 
+GPIO.setmode(GPIO.BCM)
+
+buttonPin = 7
+shutDown = 8
 ip = 'localhost'
 port = 8080
 page = '/ntap/response'
 
+GPIO.setup(buttonPin, GPIO.IN, pull_up_up = GPIO.PUD_UP)
+GPIO.setup(shutDown, GPIO.IN, pull_up_up = GPIO.PUD_UP)
+
 response = printer.PrinterResponse(ip, port, page)
 
-response.send()
-# response.asyncSend()
+while True:
+    if GPIO.input(buttonPin):
+        print("Done Printing")
+        response.send()
+    if GPIO.input(shutDown):
+        print("Shutdown")
+        os.system("shutdown now -h")
+        sys.exit(0)
