cd0b452dc205a2d7503d9048944041009a99a4a6
Tim <pi@raspberrypi.(none)>
2015-08-13 20:35:37 +0000

    Checks board model, shutdown button implemented, button checking better

--- main.py
+++ main.py
@@ -5,34 +5,100 @@ import sys
 import time
 import datetime
 
+# ============
+#   Settings
+# ============
+
 GPIO.setmode(GPIO.BOARD)
 
 ip = '192.168.1.22'
 port = 80
 page = '/NameTag/ntap/response'
 
-buttonPin = 24
-shutDown = 26
-pressed = False
+#Pre B+
+bDonePin = 24
+bHaltPin = 26
 
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-GPIO.setup(shutDown, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#B+ and Pi 2
+pDonePin = 38
+pHaltPin = 40
+
+doneDelay = 0
+haltDelay = 1000
+
+# ================
+#   End Settings
+# ================
+
+# Setup
+
+donePin = bDonePin
+haltPin = bHaltPin
+
+# Get time in milliseconds
+def millis():
+	return int(round(time.time()*1000))
+
+# Log a mesage
+def log(msg):
+	print(datetime.datetime.now().strftime("%I:%M:%S:%f")+": "+msg)
+
+# Called when done button is pressed
+def done():
+	log("Done")
+	#response.send()
+
+# Called when halt button is pressed
+def halt():
+	log("Halt")
+
+if GPIO.RPI_REVISION >= 3:
+	donePin = pDonePin
+	haltPin = pHaltPin
+	print("B+ or Pi 2")
+else:
+	print("Pi 1 not +")
+
+donePressed = False
+haltPressed = False
+doneMillis = 0;
+haltMillis = 0;
+
+GPIO.setup(donePin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(haltPin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
+GPIO.add_event_detect(donePin, GPIO.BOTH)
+GPIO.add_event_detect(haltPin, GPIO.BOTH)
 
 response = printer.PrinterResponse(ip, port, page)
 
+# Main Loop
+
 while True:
-    if not GPIO.input(buttonPin) and not pressed:
-        print(datetime.datetime.now().strftime("\n%I:%M:%S%p on %B %d, %Y"))
-        pressed = True
-        print("Done Printing")
-        response.send()
-        time.sleep(1)
-    # elif not GPIO.input(shutDown) and not pressed:
-    #     pressed = True
-    #     print("Shutdown")
-    #     os.system("shutdown now -h")
-    #     sys.exit(0)
-    elif GPIO.input(buttonPin)and pressed:
-        pressed = False
-        time.sleep(1)
-    #print "Button: ", GPIO.input(buttonPin), "Pressed: ", pressed
+	if GPIO.event_detected(donePin):
+		if not GPIO.input(donePin):
+			log("Done Pressed")
+			doneMillis = millis()
+			donePressed = True
+		else:
+			log("Done Released")
+			donePressed = False
+	
+	if GPIO.event_detected(haltPin):
+		if not GPIO.input(haltPin):
+			log("Halt Pressed")
+			haltMillis = millis()
+			haltPressed = True
+		else:
+			log("Halt Released")
+			haltPressed = False
+	
+	if donePressed:
+		if millis() - doneMillis >= doneDelay:
+			donePressed = False
+			done()
+
+	if haltPressed:
+		if millis() - haltMillis >= haltDelay:
+			haltPressed = False
+			halt()
