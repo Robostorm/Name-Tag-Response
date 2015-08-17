05e4d2ccfb80f9077b4a44ae5654a44180da72a6
zethra <jediben97@gmail.com>
2015-08-16 20:01:00 -0400

    Added to install script and added remove script

--- install-daemon.sh
+++ install-daemon.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
-   exit 1
-fi
-
+mkdir /opt/ntrs
+cp * /opt/ntrs
 cp ntrs /etc/init.d/ntrs
 chmod +x /etc/init.d/ntrs
 update-rc.d ntrs defaults
---
+++ remove-daemon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -r /opt/ntrs
+rm /etc/init.d/ntrs
+update-rc.d ntrs remove
