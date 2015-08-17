488d9ebb029a8f00c465c2e9060677afedfd5f30
zethra <jediben97@gmail.com>
2015-08-17 18:16:06 -0400

    Added start and stop messagee to daemon

--- ntrs.py
+++ ntrs.py
@@ -9,10 +9,13 @@ if __name__ == '__main__':
     daemon = NTDaemon('/tmp/ntdaemon.pid', stdout='/var/log/ntrout.log', stderr='/var/log/ntrerr.log')
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
+            print "Staring Name Tag Response Server..."
             daemon.start()
         elif 'stop' == sys.argv[1]:
+            print "Stopping Name Tag Response Server..."
             daemon.stop()
         elif 'restart' == sys.argv[1]:
+            print "Restarting Name Tag Response Server..."
             daemon.restart()
         else:
             print "Unknown command"
